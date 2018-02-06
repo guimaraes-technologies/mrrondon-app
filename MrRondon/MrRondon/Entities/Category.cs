b3562b971a6e6dd159a04e3068c83e993d1309ce
Oziel Guimaraes <contato@ozielguimaraes.net>
2018-02-06 16:44:42 -0400

    Fix image on categories list

--- MrRondon/MrRondon/Entities/Category.cs
+++ MrRondon/MrRondon/Entities/Category.cs
@@ -8,9 +8,8 @@ namespace MrRondon.Entities
         public int CategoryId { get; set; }
         public string Name { get; set; }
         public byte[] Image { get; set; }
-        public string ImageString { get; set; }
 
-        public ImageSource GetImage { get { return string.IsNullOrWhiteSpace(ImageString) ? ImageSource.FromFile("icon.png") : ImageSource.FromStream(() => new MemoryStream(Image)); } }
+        public ImageSource ImageSource { get { return Image == null ? ImageSource.FromFile("sad_face") : ImageSource.FromStream(() => new MemoryStream(Image)); } }
 
         public int? SubCategoryId { get; set; }
         public Category SubCategory { get; set; }
