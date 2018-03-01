7d0df6b739179104f2d7abdc5c34529780be476a
Oziel Guimaraes <microzapple@gmail.com>
2018-03-01 13:09:31 -0400

    Increase area to get plages

--- MrRondon/MrRondon/Helpers/Constants.cs
+++ MrRondon/MrRondon/Helpers/Constants.cs
@@ -8,7 +8,7 @@
         //public static string Host => "http://localhost:1111";
         public const string AppName = "Mr Rondon Turismo";
         //Distance in meters to get places nearby
-        public const int GetPlacesUntil = 1000;
+        public const int GetPlacesUntil = 5000;
         public const string TokenType = "Bearer";
         public const string ClientId = "mrrondon.app";
         public const string ClientSecret = "Mr.Rondon.Turismo.App";
