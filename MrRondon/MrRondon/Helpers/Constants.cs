0ee381b787cfadd582e53c9c4a8ffe3ceb934799
Oziel Guimaraes <microzapple@gmail.com>
2018-03-06 09:38:25 -0400

    Added check and trash icon

--- MrRondon/MrRondon/Helpers/Constants.cs
+++ MrRondon/MrRondon/Helpers/Constants.cs
@@ -7,8 +7,6 @@
 
         //public static string Host => "http://192.168.56.1:1111";
         public const string AppName = "Mr Rondon Turismo";
-        //Distance in meters to get places nearby
-        public const int GetPlacesUntil = 5000;
         public const string TokenType = "Bearer";
         public const string ClientId = "mrrondon.app";
         public const string ClientSecret = "Mr.Rondon.Turismo.App";
